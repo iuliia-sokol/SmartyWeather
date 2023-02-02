0cf0f5df69ba58d7e11a0b01f2bee59cf8f6aff5
Iuliia Sokol <avare779@gmail.com>
2023-02-02 18:53:51 +0200

    added useMedia hook

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,13 +4,16 @@ import { fetchPexelsImage } from 'redux/location/locOperations';
 import { useState } from 'react';
 import { Card } from './Card.styled';
 import { getCityImages, getCityName } from 'redux/location/locSelectors';
+import { useMediaQuery } from 'hooks/useMedia';
 
 export function CardUI() {
+  const isRowBased = useMediaQuery('(min-width: 500px)');
   const city = useSelector(getCityName);
   const images = useSelector(getCityImages);
   const defaultImg =
     'https://www.wallpaperflare.com/static/79/210/459/nature-sky-umbrella-red-wallpaper-preview.jpg';
   const [image, setImage] = useState('');
+  const [imageMob, setImageMob] = useState('');
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -22,17 +25,21 @@ export function CardUI() {
   useEffect(() => {
     if (images.length === 1) {
       setImage(images[0].landscape);
+      setImageMob(images[0].portrait);
     }
     if (images.length > 1) {
       const random = Math.floor(Math.random() * images.length);
       setImage(images[random].landscape);
+      setImageMob(images[random].portrait);
     }
   }, [images]);
 
   return (
     <Card
       style={{
-        backgroundImage: `url(${image ?? defaultImg})`,
+        backgroundImage: !isRowBased
+          ? `url(${imageMob ?? defaultImg})`
+          : `url(${image ?? defaultImg})`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
       }}
