581ca7e4a4fcb73a12797b0c3ca1dbdf08f4b636
Iuliia Sokol <avare779@gmail.com>
2023-02-05 16:00:30 +0200

    loader styles edited

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -16,48 +16,47 @@ export const LoaderOverlay = styled.div`
 
 export const LoaderWrapper = styled.div`
   position: absolute;
-  /* top: 30%; */
-  top: 20%;
-  /* width: 180px;
-  height: 180px; */
+  top: 40%;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+
+  @media screen and (min-width: 1280px) {
+    top: 20%;
+  }
 `;
 
 const breatheAnimation = keyframes`
  0% {
     transform: translate(0, 0) rotate(0deg);
-     
   }
   25% {
-    transform:   translate(70%, 50%) rotate(90deg);
-     
+    transform:   translate(70%, 50%) rotate(90deg);   
   }
   50% {
-    transform:  translate(0, 50%) rotate(180deg);
-      
+    transform:  translate(0, 50%) rotate(180deg);    
   }
    75% {
     transform: translate(-70%, 50%) rotate(270deg);
-    
   }
   100% {
     transform:   translate(0, 0) rotate(360deg);
-      
   }
 `;
 
 export const PicWrapper = styled.div`
   position: absolute;
   display: flex;
-  /* flex-direction: column; */
   justify-content: center;
   align-items: center;
   gap: 16px;
 
   & img {
-    width: 10vw;
+    width: 25vw;
     animation: ${breatheAnimation} 2.5s linear infinite;
+
+    @media screen and (min-width: 1280px) {
+      width: 10vw;
+    }
   }
 `;
