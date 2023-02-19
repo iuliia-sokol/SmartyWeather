47067df6cafbb13f84849f5d85860c63af31518c
Iuliia Sokol <avare779@gmail.com>
2023-02-19 11:54:41 +0200

    header styles edited

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,22 @@
 import { Container } from 'components/Container/Container';
 import { HistoryLink } from 'components/HistoryLink/HistoryLink';
 import { Logo } from 'components/Logo/Logo';
-import { HeaderWrapper, LogoWrapper } from './Header.styled';
+import {
+  HeaderContentWrapper,
+  HeaderWrapper,
+  LogoWrapper,
+} from './Header.styled';
 
 export const Header = () => {
   return (
     <HeaderWrapper>
       <Container>
-        <LogoWrapper>
-          <Logo />
+        <HeaderContentWrapper>
+          <LogoWrapper>
+            <Logo />
+          </LogoWrapper>
           <HistoryLink />
-        </LogoWrapper>
+        </HeaderContentWrapper>
       </Container>
     </HeaderWrapper>
   );
--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 
 export const HeaderWrapper = styled.header``;
 
+export const HeaderContentWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
 export const LogoWrapper = styled.div`
   display: flex;
 `;
